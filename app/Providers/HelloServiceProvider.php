87d523f42a18aa390bd965ca28d5da5a5f354ae9
3509nkf <nagokaitofabian@gmail.com>
2021-09-20 16:39:40 +0900

    app

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -4,15 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Validator;
+use App\Http\Validators\HelloValidator;
+use Illuminate\Http\Requests\HelloRequest;
+ 
+
 
 class HelloServiceProvider extends ServiceProvider
 {
     
     public function boot()
     {
-        View::composer(
-           'hello.index', 'App\Http\Composers\HelloComposer'
-           
-        );
+        $validator = $this->app['validator'];
+        $validator->resolver(function($translator, $data, $rules, $messages) {
+               return new HelloValidator($translator, $data, $rules, $messages);
+        });
     }
 }
