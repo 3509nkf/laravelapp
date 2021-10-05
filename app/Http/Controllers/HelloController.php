1720b994d0e9256913d61a053655d203660f8c2a
3509nkf <nagokaitofabian@gmail.com>
2021-10-06 01:19:50 +0900

    app

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Validator;
-use Illuminate\Http\Requests\HelloRequest;
+use App\Http\Requests\HelloRequest;
  
 
 
@@ -14,12 +14,27 @@ class HelloController extends Controller
 
       
      public function index(Request $request){
-            return view('hello.index', ['msg'=>'フォームを入力して下さい。']);
+           
+            if($request->hasCookie('msg'))
+            {
+                $msg = 'Cookie: ' . $request->cookie('msg');
+            }else {
+                 $msg = '*クッキーはありません。';
+            }return view('hello.index', ['msg'=> $msg]);
     }
  
-     public function post(HelloRequest $request) {
-           return view('hello.index',['msg'=>'正しく入力されました!']);
-      }
+     public function post(Request $request) {
+         
+           $validate_rule = [ 
+                'msg' => 'required',
+           ];
+           $this->validate($request, $validate_rule);
+           $msg = $request->msg;
+           $response = response()->view('hello.index',
+                  ['msg'=>'[' . $msg . '」を保存しました。']);
+           $response->cookie('msg', $msg, 100);
+           return $response;
+     }
 
     
 }
