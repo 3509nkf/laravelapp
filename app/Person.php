1960b48f586218ff679115353f95b50c706ab887
3509nkf <nagokaitofabian@gmail.com>
2022-01-18 18:17:01 +0900

    app

--- app/Person.php
+++ app/Person.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+      'name' => 'required',
+      'mail' => 'email',
+      'age'  => 'integer|min:0|max:150'
+    );
+
     public function getData() {
      	return $this->id . ': ' . $this->name . '(' . $this->age . ')';
     }
@@ -23,4 +34,12 @@ class Person extends Model
     {
        return $query->where('age', '<=', $n);
     }
+
+    protected static function boot()
+    {
+      parent::boot();
+      static::addGlobalScope(new ScopePerson);
+    }
+
+   
 }
