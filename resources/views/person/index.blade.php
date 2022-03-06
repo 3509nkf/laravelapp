169595da74d838481645140410d63feb69f2b0d5
3509nkf <nagokaitofabian@gmail.com>
2022-03-06 17:00:28 +0900

    app

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -10,18 +10,25 @@
 @section('content')
 	<table>
 	<tr><th>Person</th><th>Board</th></tr>
-        @foreach ($items as $item)
+        @foreach ($hasItems as $item)
 		<tr>
                     <td>{{$item->getData()}}</td>
-                    <td>@if ($item->boards != null)
+                    <td>
                         <table width="100%">
                         @foreach ($item->boards as $obj)
-                        	<tr><td>{{$obj->getData()}}
-                     	</td></tr>
-                        @endforeach
+                            <tr><td>{{$obj->getData()}}</td></tr>
+                        @endreach
                         </table>
-                        @endif
                     </td>
+                </tr>
+        @endforeach
+        </table>
+        <div style = "margin:10px;"></div>
+        <table>
+        <tr><th>Person</th></tr>
+        @foreach ($noItems as $item)
+        	<tr> 
+                 	<td>{{$item->getData()}}</td>
                 </tr>
         @endforeach
         </table>
